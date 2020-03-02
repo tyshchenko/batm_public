e4bb4c55c627fa6de4ec33e17f8d451327fbd270
Viktor <vtitch@gmail.com>
2020-03-02 15:45:37 +0100

    Update DashRPCWallet.java

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/dash/wallets/dashd/DashRPCWallet.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/dash/wallets/dashd/DashRPCWallet.java
@@ -22,8 +22,8 @@ import com.generalbytes.batm.server.extensions.extra.common.RPCWallet;
 
 //You need to have node running: i.e.:  bitcoind -rpcuser=rpcuser -rpcpassword=rpcpassword -rpcport=8332
 
-public class BitcoinCashRPCWallet extends RPCWallet {
-    public BitcoinCashRPCWallet(String rpcURL, String accountName) {
-        super(rpcURL, accountName, CryptoCurrency.BCH.getCode());
+public class DashRPCWallet extends RPCWallet {
+    public DashRPCWallet(String rpcURL, String accountName) {
+        super(rpcURL, accountName, CryptoCurrency.DASH.getCode());
     }
 }
