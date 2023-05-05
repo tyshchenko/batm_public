c0d08d891264d10c26a0039c618fc239b08e631d
Viktor <wmwizard@gmail.com>
2023-05-05 15:03:34 +0200

    usdt

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/ethereum/etherscan/TokenScan.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/ethereum/etherscan/TokenScan.java
@@ -1,7 +1,6 @@
 package com.generalbytes.batm.server.extensions.extra.ethereum.etherscan;
 
 import si.mazi.rescu.RestProxyFactory;
-import com.generalbytes.batm.server.extensions.extra.ethereum.etherscan.EtherScan;
 import java.math.BigDecimal;
 
 public class TokenScan {
@@ -9,9 +8,18 @@ public class TokenScan {
     protected IOurAPI ourApi = RestProxyFactory.createProxy(IOurAPI.class, "http://127.0.0.1:8099/");
 
 
-    public EtherScan.AddressBalance getTokenBalance(String address, String cryptoCurrency, String label, BigDecimal amount) {
+    public AddressBalance getTokenBalance(String address, String cryptoCurrency, String label, BigDecimal amount) {
         BalanceData result = ourApi.getlabelstatus(cryptoCurrency, address, label, amount.toString());
-        return new EtherScan.AddressBalance(result.getBalance(), result.getConfirmation());
+        return new AddressBalance(result.getBalance(), result.getConfirmation());
     }
 
+    public class AddressBalance {
+        public final BigDecimal receivedAmount;
+        public final int confirmations;
+
+        public AddressBalance(BigDecimal receivedAmount, int confirmations) {
+            this.receivedAmount = receivedAmount;
+            this.confirmations = confirmations;
+        }
+    }
 }
